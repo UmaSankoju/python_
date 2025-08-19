205789f5eebcfdaceae5a1e5ca167f6ee9f4498e
UmaSankoju <sankojuuma25@gmail.com>
2025-08-19 11:22:27 +0530

    secondpush

--- .vscode/problemsolving.py
+++ .vscode/problemsolving.py
@@ -8,7 +8,26 @@ while n>0:
     sum+=digit**digits
     n=n//10
 print(sum)
+
+
 if sum==temp:
     print("armstrong number")
 else:
-    print(temp,"is not armstron number")    
+    print(temp,"is not armstron number")  
+    
+###USING FUNCTIONS###    
+    
+def check_armstrong_number(n):
+    temp=n
+    sum=0
+    digits=len(str(n)) 
+    while n>0:
+        digit=n%10
+        sum+=digit**digits
+        n=n//10 
+    
+    return "armstrong number" if sum==temp else "it is not an armstrong number"
+
+      
+n=int(input("enter a number:")) 
+print(check_armstrong_number(n))
