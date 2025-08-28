091509e5b5acbe4c9b7c6c3c449ef6ba5a4ea768
UmaSankoju <sankojuuma25@gmail.com>
2025-08-28 14:49:30 +0530

    in_buit(string)

---
+++ .vscode/in_built_string.py
@@ -0,0 +1,77 @@
+string=" Python String In Built Functions"
+print("length of string:")
+print(len(string))
+print("Checking & Validation")
+print()
+print("1.checking characters are alphabets or not")
+string=" Python String In Built Functions"
+print(string.isalpha())
+print()
+print("2.checking characters are numbers or not")
+string="90978654323546789"
+print(string.isdigit())
+print()
+print("3.checking characters are alphanumeric or not")
+string=" Python String In Built Functions 123"
+print(string.isalnum())
+print()
+print("4.checks if string has only whitespace")
+string=" Python String In Built Functions"
+print(string.isspace())
+print()
+print("5.checks all characters are in Uppercase")
+string=" Python String In Built Functions"
+print(string.isupper())
+print()
+print("6.checks all characters are in lowercase")
+string=" Python String In Built Functions"
+print(string.islower())
+print()
+print("7.checks if string follows title case or not")
+string=" Python String In Built Functions"
+print(string.istitle())
+print()
+print("8.checks if string starts with subsring")
+string="Python String In Built Functions"
+print(string.startswith("python"))
+print()
+print("9.checks if string ends with subsring")
+string="Python String In Built Functions"
+print(string.startswith("Built"))
+print()
+print("searching")
+print()
+string=" Python string In Built Functions"
+print(string.find("string"))
+print(string.rfind("string")) 
+print(string.index("string")) 
+print(string.rindex("string"))
+print(string.count("u") )
+print()
+print("Modification")
+print()
+string=" Python String In Built Functions"
+print(string.lower())
+print(string.upper())
+print(string.title())
+print(string.capitalize())
+print(string.swapcase())
+print(string.strip())
+print(string.lstrip())
+print(string.rstrip())
+print(string.replace("python","java"))
+print()
+print("Splitting & Joining")
+print()
+string=" Python String In Built Functions "
+print(string.split())
+print(string.rsplit())
+print(string.splitlines())
+
+
+
+
+
+
+
+
